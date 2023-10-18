57916863009b0e7e0609bb19d1117fc68d66b120
simouzel1964 <147699970+simouzel1964@users.noreply.github.com>
2023-10-18 16:35:10 +0300

    Add files via upload
    my nextjs blog

---
+++ out/_next/static/ApIEpCyxcdtZ8bmZLNgS1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a6890a3306711e32.css","static/chunks/pages/index-97525ae965139428.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
